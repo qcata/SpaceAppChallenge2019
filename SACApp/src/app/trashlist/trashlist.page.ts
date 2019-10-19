7aa3366952789be2891e9b88bcea81bed211da1e
boogie <tereanubogdan@yahoo.com>
2019-10-20 02:45:46 +0300

    - added  image buttons

--- SACApp/src/app/trashlist/trashlist.page.ts
+++ SACApp/src/app/trashlist/trashlist.page.ts
@@ -11,29 +11,46 @@ import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 export class TrashlistPage implements OnInit {
   public trashlist = [
     {
+      imageLink:'https://static.thenounproject.com/png/43279-200.png',
       trashName: 'Cigarette',
-      description: 'BlaBla'
+      points: 1
     },
     {
-      trashName: 'Plastic bottle',
-      description: 'BlaBla'
+      imageLink:'https://static.thenounproject.com/png/20716-200.png',
+      trashName: 'Food Wrappers',
+      points: 1
     },
     {
-      trashName: 'Straws',
-      description: 'BlaBla'
+      imageLink:'https://icon-library.net/images/water-bottle-icon-png/water-bottle-icon-png-14.jpg',
+      trashName: 'Plastic Bottle',
+      points: 1
     },
     {
+      imageLink:'https://library.kissclipart.com/20180830/bye/kissclipart-soft-drink-clipart-fizzy-drinks-coca-cola-1907d73f1a6a0de2.png',
       trashName: 'Cans',
-      description: 'BlaBla'
+      points: 1
     },
     {
-      trashName: 'Glass',
-      description: 'BlaBla'
+      imageLink:'https://icon-library.net/images/straw-icon/straw-icon-22.jpg',
+      trashName: 'Straw',
+      points: 1
     },
     {
+      imageLink:'https://cdn2.iconfinder.com/data/icons/waste-management-and-recycling/64/plastic_bag-512.png',
       trashName: 'Plastic bags',
-      description: 'BlaBla'
+      points: 1
+    },
+    {
+      imageLink:'https://cdn2.iconfinder.com/data/icons/food-solid-icons-volume-1/128/045-512.png',
+      trashName: 'Plates',
+      points: 1
+    },
+    {
+      imageLink:'http://pngimages.net/sites/default/files/trash-logo-png-image-83522.png',
+      trashName: 'Other',
+      points: 1
     }
+
   ];
   trashList = this.trashlist;
   halfLength = Math.ceil(this.trashList.length / 2);
