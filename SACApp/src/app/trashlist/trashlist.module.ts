16dbc82c912c8c9420bea704987c7fb13009093d
unknown <tereanubogdan@yahoo.com>
2019-10-19 19:36:54 +0300

    - added share

--- SACApp/src/app/trashlist/trashlist.module.ts
+++ SACApp/src/app/trashlist/trashlist.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -21,6 +22,7 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [TrashlistPage]
+  declarations: [TrashlistPage],
+  providers: [SocialSharing ]
 })
 export class TrashlistPageModule {}
--- SACApp/src/app/trashlist/trashlist.page.ts
+++ SACApp/src/app/trashlist/trashlist.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-trashlist',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrashlistPage implements OnInit {
 
-  constructor() { }
+  constructor(private socialSharing: SocialSharing) { }
 
   ngOnInit() {
   }
+
+  share(){
+    this.socialSharing.shareViaFacebook("you has x points","arraylike string","url");
+  }
+
 
 }
