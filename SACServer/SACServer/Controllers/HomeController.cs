71e87062e419926e51544c1d03bf02298b059561
Catalin <catalin.mcm13@gmail.com>
2019-10-20 12:03:39 +0300

    - added dummy nasa service

--- SACServer/SACServer/Controllers/HomeController.cs
+++ SACServer/SACServer/Controllers/HomeController.cs
@@ -5,13 +5,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SACServer.Models;
+using SACServer.Services;
 
 namespace SACServer.Controllers
 {
     public class HomeController : Controller
     {
+        private NasaService _nasaService;
+
+        public HomeController()
+        {
+            _nasaService = new NasaService();
+        }
+
         public IActionResult Index()
         {
+            var nasaApiRespone = Task.Run(async () => await _nasaService.GetNasaDataFromAPIAsync()).Result;
             return View();
         }
 
---
+++ SACServer/SACServer/Services/NasaService.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SACServer.Services
+{
+    public class NasaService
+    {
+        public async Task<string> GetNasaDataFromAPIAsync()
+        {
+            string something = string.Empty;
+            string baseUrl = "https://podaac.jpl.nasa.gov/ws/search/granule/?datasetId=PODAAC-AKASA-XOGD1";
+            using (HttpClient client = new HttpClient())
+
+            using (HttpResponseMessage res = await client.GetAsync(baseUrl))
+            using (HttpContent content = res.Content)
+            {
+                string data = await content.ReadAsStringAsync();
+                if (data != null)
+                {
+                    something = data.ToString();
+                }
+            }
+            return something;
+        }
+    }
+}
